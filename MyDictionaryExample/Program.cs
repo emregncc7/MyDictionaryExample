b65a0e5450829ef38881d5d30768672f0b65bfbc
emre genc <emregenc696@outlook.com>
2022-12-30 00:56:25 +0300

    Add project files.

---
+++ MyDictionaryExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyDictionaryExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> mydictionary = new MyDictionary<string>();
+
+            mydictionary.Add("ADD");
+        }
+    }
+    class MyDictionary<T>
+    {
+        T[] array;
+        T[] _tempArray;
+        public MyDictionary()
+        {
+            array = new T[0];
+        }
+
+        public void Add(T item) 
+        {
+            _tempArray = array;
+            array = new T[_tempArray.Length+1];
+            for (int i = 0; i < array.Length; i++)
+            {
+                _tempArray[i] = array[i];
+            }
+
+            array[array.Length - 1] = item;
+        }
+    }
+}
